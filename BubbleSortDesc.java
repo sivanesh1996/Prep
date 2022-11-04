9515aac23c5711be7872257dc64e65cf8624475a
sivanesh1996 <111571530+sivanesh1996@users.noreply.github.com>
2022-11-04 17:52:27 +0530

    Bubble sort-descending

---
+++ BubbleSortDesc.java
@@ -0,0 +1,24 @@
+package prep;
+
+public class BubbleSortDesc {
+
+	public static void main(String[] args) {
+int ar[]= {7,5,1,4,6,3,8,16};
+		
+		for(int i=0;i<ar.length;i++) {
+			for(int j=i+1;j<ar.length;j++) {
+				if(ar[i]<ar[j]) {
+				int temp=ar[i];
+				ar[i]=ar[j];
+				ar[j]=temp;
+				}
+			}
+		}
+		
+		for(int o:ar) {
+			System.out.print(o +" ");
+		}
+
+	}
+
+}
